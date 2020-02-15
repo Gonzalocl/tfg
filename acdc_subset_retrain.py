a02334e787ff90d630939183f4beb5d45c898746
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-02-16 00:40:40 +0100

    create image list subset retrain

--- acdc_subset_retrain.py
+++ acdc_subset_retrain.py
@@ -192,67 +192,56 @@ def create_image_lists(image_dir, subsets, testing_percentage, validation_percen
           return None
       subsets_list.append(subset_list)
 
-  is_root_dir = True
-  for sub_dir in sub_dirs:
-    if is_root_dir:
-      is_root_dir = False
-      continue
+  for subset_list in subsets_list:
     extensions = sorted(set(os.path.normcase(ext)  # Smash case on Windows.
                             for ext in ['JPEG', 'JPG', 'jpeg', 'jpg', 'png']))
-    file_list = []
-    dir_name = os.path.basename(
-        # tf.gfile.Walk() returns sub-directory with trailing '/' when it is in
-        # Google Cloud Storage, which confuses os.path.basename().
-        sub_dir[:-1] if sub_dir.endswith('/') else sub_dir)
-
-    if dir_name == image_dir:
-      continue
-    logging.info("Looking for images in '%s'",  dir_name)
-    for extension in extensions:
-      file_glob = os.path.join(image_dir, dir_name, '*.' + extension)
-      file_list.extend(tf.gfile.Glob(file_glob))
-    if not file_list:
-      logging.warning('No files found')
-      continue
-    if len(file_list) < 20:
-      logging.warning(
-          'WARNING: Folder has less than 20 images, which may cause issues.')
-    elif len(file_list) > MAX_NUM_IMAGES_PER_CLASS:
-      logging.warning(
-          'WARNING: Folder %s has more than %s images. Some images will '
-          'never be selected.', dir_name, MAX_NUM_IMAGES_PER_CLASS)
-    label_name = re.sub(r'[^a-z0-9]+', ' ', dir_name.lower())
+    label_name = "_".join([re.sub(r'[^a-z0-9]+', ' ', dir_name.lower()) for dir_name in subset_list])
     training_images = []
     testing_images = []
     validation_images = []
-    for file_name in file_list:
-      base_name = os.path.basename(file_name)
-      base_name = os.path.join(dir_name, base_name)
-      # We want to ignore anything after '_nohash_' in the file name when
-      # deciding which set to put an image in, the data set creator has a way of
-      # grouping photos that are close variations of each other. For example
-      # this is used in the plant disease data set to group multiple pictures of
-      # the same leaf.
-      hash_name = re.sub(r'_nohash_.*$', '', file_name)
-      # This looks a bit magical, but we need to decide whether this file should
-      # go into the training, testing, or validation sets, and we want to keep
-      # existing files in the same set even if more files are subsequently
-      # added.
-      # To do that, we need a stable way of deciding based on just the file name
-      # itself, so we do a hash of that and then use that to generate a
-      # probability value that we use to assign it.
-      hash_name_hashed = hashlib.sha1(tf.compat.as_bytes(hash_name)).hexdigest()
-      percentage_hash = ((int(hash_name_hashed, 16) %
-                          (MAX_NUM_IMAGES_PER_CLASS + 1)) *
-                         (100.0 / MAX_NUM_IMAGES_PER_CLASS))
-      if percentage_hash < validation_percentage:
-        validation_images.append(base_name)
-      elif percentage_hash < (testing_percentage + validation_percentage):
-        testing_images.append(base_name)
-      else:
-        training_images.append(base_name)
+    for dir_name in subset_list:
+      file_list = []
+      logging.info("Looking for images in '%s'", dir_name)
+      for extension in extensions:
+        file_glob = os.path.join(image_dir, dir_name, '*.' + extension)
+        file_list.extend(tf.gfile.Glob(file_glob))
+      if not file_list:
+        logging.warning('No files found')
+        continue
+      if len(file_list) < 20:
+        logging.warning(
+          'WARNING: Folder has less than 20 images, which may cause issues.')
+      elif len(file_list) > MAX_NUM_IMAGES_PER_CLASS:
+        logging.warning(
+          'WARNING: Folder %s has more than %s images. Some images will '
+          'never be selected.', dir_name, MAX_NUM_IMAGES_PER_CLASS)
+      for file_name in file_list:
+        base_name = os.path.basename(file_name)
+        base_name = os.path.join(dir_name, base_name)
+        # We want to ignore anything after '_nohash_' in the file name when
+        # deciding which set to put an image in, the data set creator has a way of
+        # grouping photos that are close variations of each other. For example
+        # this is used in the plant disease data set to group multiple pictures of
+        # the same leaf.
+        hash_name = re.sub(r'_nohash_.*$', '', file_name)
+        # This looks a bit magical, but we need to decide whether this file should
+        # go into the training, testing, or validation sets, and we want to keep
+        # existing files in the same set even if more files are subsequently
+        # added.
+        # To do that, we need a stable way of deciding based on just the file name
+        # itself, so we do a hash of that and then use that to generate a
+        # probability value that we use to assign it.
+        hash_name_hashed = hashlib.sha1(tf.compat.as_bytes(hash_name)).hexdigest()
+        percentage_hash = ((int(hash_name_hashed, 16) %
+                            (MAX_NUM_IMAGES_PER_CLASS + 1)) *
+                           (100.0 / MAX_NUM_IMAGES_PER_CLASS))
+        if percentage_hash < validation_percentage:
+          validation_images.append(base_name)
+        elif percentage_hash < (testing_percentage + validation_percentage):
+          testing_images.append(base_name)
+        else:
+          training_images.append(base_name)
     result[label_name] = {
-        'dir': dir_name,
         'training': training_images,
         'testing': testing_images,
         'validation': validation_images,
@@ -288,7 +277,6 @@ def get_image_path(image_lists, label_name, index, image_dir, category):
                   label_name, category)
   mod_index = index % len(category_list)
   base_name = category_list[mod_index]
-  sub_dir = label_lists['dir']
   full_path = os.path.join(image_dir, base_name)
   return full_path
 
@@ -429,7 +417,9 @@ def get_or_create_bottleneck(sess, image_lists, label_name, index, image_dir,
     Numpy array of values produced by the bottleneck layer for the image.
   """
   label_lists = image_lists[label_name]
-  sub_dir = label_lists['dir']
+  category_list = label_lists[category]
+  mod_index = index % len(category_list)
+  sub_dir = os.path.dirname(category_list[mod_index])
   sub_dir_path = os.path.join(bottleneck_dir, sub_dir)
   ensure_dir_exists(sub_dir_path)
   bottleneck_path = get_bottleneck_path(image_lists, label_name, index,
