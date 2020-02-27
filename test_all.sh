bce09365fa745129989162654239dcfe4019c92f
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-02-27 20:57:59 +0100

    test all

--- test_all.sh
+++ test_all.sh
@@ -7,6 +7,8 @@
 ./acdc_subset_test.sh
 ./acdc_cascade_test.sh "output/04_acdc_cascade_test"
 
+
 ./acdc_distortion.sh
-./acdc_test.sh
+
+./acdc_test.sh "output/01_distortion_acdc_test"
 ./acdc_one_slice_test.sh
