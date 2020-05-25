3585979daaeb02d266324f19113dbfa0a61d800c
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-05-25 20:06:53 +0200

    out funciton

--- summary.sh
+++ summary.sh
@@ -8,15 +8,20 @@ function inference_accuracy {
   echo "$test,$accuracy"
 }
 
+function out_accuracy {
+  test="$1"
+  results_file="$2"
+  accuracy=$(grep "Final test accuracy" "$results_file" | cut -d '%' -f 1 | cut -d '=' -f 2 | tr -d [:blank:])
+  echo "$test,$accuracy"
+}
+
 echo test,accuracy
 
 
 output_dir="output/00_flowers_test"
 results_dir="$output_dir/r"
 if [[ -d $results_dir ]]; then
-  test="00_flowers_test"
-  accuracy=$(grep "Final test accuracy" "$results_dir/out" | cut -d '%' -f 1 | cut -d '=' -f 2 | tr -d [:blank:])
-  echo "$test,$accuracy"
+  out_accuracy "00_flowers_test" "$results_dir/out"
 fi
 
 
