b05c9f3e652636266a70ead2bf44ceb194a1a27c
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-05-25 20:02:02 +0200

    function inference

--- summary.sh
+++ summary.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+function inference_accuracy {
+  test="$1"
+  results_file="$2"
+  accuracy=$(tail -n 1 "$results_file" | cut -d '%' -f 1 | cut -d ':' -f 2 | tr -d [:blank:])
+  echo "$test,$accuracy"
+}
+
 echo test,accuracy
 
 
@@ -12,3 +19,9 @@ if [[ -d $results_dir ]]; then
   echo "$test,$accuracy"
 fi
 
+
+results_dir="output/01_acdc_test/r"
+if [[ -d $results_dir ]]; then
+  inference_accuracy "acdc_test" "$results_dir/output_inference"
+  inference_accuracy "acdc_test_per_patient_prediction" "$results_dir/output_per_patient_prediction"
+fi
