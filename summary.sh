5e7270737991c52fde6bc870917f14d3c2e76834
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-05-25 21:03:20 +0200

    disctoricon

--- summary.sh
+++ summary.sh
@@ -46,3 +46,27 @@ if [[ -d $results_dir ]]; then
   inference_accuracy "04_acdc_cascade_test" "$results_dir/testing_output_inference"
   inference_accuracy "04_acdc_cascade_test_per_patient_prediction" "$results_dir/testing_output_per_patient_prediction"
 fi
+
+results_dir="output/01_distortion_acdc_test/r"
+if [[ -d $results_dir ]]; then
+  inference_accuracy "01_distortion_acdc_test" "$results_dir/output_inference"
+  inference_accuracy "01_distortion_acdc_test_per_patient_prediction" "$results_dir/output_per_patient_prediction"
+fi
+
+results_dir="output/02_distortion_acdc_one_slice_test/r"
+if [[ -d $results_dir ]]; then
+  out_accuracy "02_distortion_acdc_one_slice_test" "$results_dir/out"
+fi
+
+for c in DCM HCM MINF NOR RV; do
+  results_dir="output/03_distortion_acdc_subset_test/$c/r"
+  if [[ -d $results_dir ]]; then
+    inference_accuracy "03_distortion_acdc_subset_test_$c" "$results_dir/out_inference"
+  fi
+done
+
+results_dir="output/04_distortion_acdc_cascade_test"
+if [[ -d $results_dir ]]; then
+  inference_accuracy "04_distortion_acdc_cascade_test" "$results_dir/testing_output_inference"
+  inference_accuracy "04_distortion_acdc_cascade_test_per_patient_prediction" "$results_dir/testing_output_per_patient_prediction"
+fi
