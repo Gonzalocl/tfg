00645c26ec45f77a2ca01fcd4b42d08b870b6420
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-06-28 13:15:34 +0200

    check params

--- video/build.sh
+++ video/build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+if [[ $# -ne 2 ]]; then
+  echo Error: need 2 params 1>&2
+  exit 1
+fi
+
+echo "Cuts timestamps csv file: $1"
+echo "Video folder: $2"
+
+out_dir="out"
+
+mkdir -p "$out_dir"
+
